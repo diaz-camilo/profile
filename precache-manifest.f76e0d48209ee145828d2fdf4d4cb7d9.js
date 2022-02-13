7aeef9d17c5bffc18348565dae360dfd923a30e1
diaz-camilo <diaz.camilo@icloud.com>
2022-02-13 14:32:45 +1100

    Updates

---
+++ precache-manifest.f76e0d48209ee145828d2fdf4d4cb7d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d0d4f399c033a5e3c71b0993883971",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "849ed600fcf2e9fbffd8",
+    "url": "/profile/static/css/2.1e7b42b6.chunk.css"
+  },
+  {
+    "revision": "0455c4f476d51db2d5fd",
+    "url": "/profile/static/css/main.0c42542a.chunk.css"
+  },
+  {
+    "revision": "849ed600fcf2e9fbffd8",
+    "url": "/profile/static/js/2.29c40e78.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/profile/static/js/2.29c40e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0455c4f476d51db2d5fd",
+    "url": "/profile/static/js/main.235f1e24.chunk.js"
+  },
+  {
+    "revision": "3d104a97681ddbf804fe",
+    "url": "/profile/static/js/runtime-main.a5515c0e.js"
+  }
+]);
